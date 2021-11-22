73f33e3afae3c4fe32f6b54fede5f8030de08328
bbozzay <bbozzay@gmail.com>
2021-11-22 15:49:57 -0700

    Generate blocklist

--- api/index.js
+++ api/index.js
@@ -21,6 +21,11 @@ class Generator {
 	get servicesDir() {
 		return path.resolve(`${this.dataDir}/parentalcontrol/services`);
 	}
+	async getOptions() {
+		let map = await this.mapDirectory(this.cacheDir);
+		console.log(map)
+		return map
+	}
 	getServices() {
 		console.log("services dir", this.servicesDir)
 		const dir = this.servicesDir
@@ -58,6 +63,37 @@ class Generator {
 		relativePath = path.resolve(`${this.cacheDir}/${relativePath}`)
 		fs.writeFile(`${relativePath}.txt`, data)
 	}
+
+	async mapDirectory(dir, group = null) {
+		const dirents = await fs.readdir(dir, { withFileTypes: true });
+		console.log("dirs", dirents)
+		let items = [];
+		for (const dirent of dirents) {
+			const res = path.resolve(dir, dirent.name);
+			//console.log(res)
+			if (dirent.isDirectory()) {
+				let files = await this.mapDirectory(res)
+				items.push(files)
+			} else {
+				items.push(res)
+			}
+		}
+		return items;
+	}
+	async createHostsFile(selected) {
+		let blocklistPath = path.resolve(`${path.resolve()}/blocklist`);
+		await fs.writeFile(blocklistPath, "");
+
+		let files = await this.mapDirectory(this.cacheDir)
+		files = files.flat();
+		for (const file of files) {
+			console.log("file", file)
+			let contents = await fs.readFile(file);
+			await fs.appendFile(blocklistPath, contents)
+		}
+
+
+	}
 	async generate() {
 		const categories = this.getCategories();
 		const services = this.getServices();
@@ -65,9 +101,10 @@ class Generator {
 		console.log("other", other)
 		const dir = categories.path;
 		const dir1 = services.path;
+		const dir2 = other.path;
 		const jsonFiles = await categories.files
 		const textFiles = await services.files
-		console.log("json file", dir, jsonFiles)
+		const otherFiles = other.files;
 		for (let i = 0; i < jsonFiles.length; i++) {
 			let fileLocation = path.resolve(`${dir}/${jsonFiles[i]}`);
 			let fileData = await fs.readFile(fileLocation, "utf8");
@@ -79,6 +116,13 @@ class Generator {
 			fileData = replaceLines(fileData);
 
 			await this.saveFile(`/services/${path.parse(textFiles[l]).name}`, fileData)
+		}
+		for (let k = 0; k < otherFiles.length; k++) {
+			let fileLocation = path.resolve(`${dir2}/${otherFiles[k].name}`);
+			let fileData = await fs.readFile(fileLocation, "utf8");
+			fileData = replaceLines(fileData);
+
+			await this.saveFile(`/${otherFiles[k].name}`, fileData)
 		}
 	}
 	async sources(fileName, file) {
@@ -99,5 +143,7 @@ class Generator {
 	}
 }
 const generator = new Generator();
-generator.generate();
+//generator.generate();
+//generator.createHostsFile();
+generator.getOptions();
 //generator.fetchData();
